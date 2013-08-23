7c9fe034632d965dac7ff9a3ff59fd7c481f4ca6
Jordan Ramey <rameyj05@gmail.com>
2013-08-23 15:38:04 -0400

    First Commit

---
+++ board.js
@@ -0,0 +1,33 @@
+﻿function Board(knight) {
+    this.knight = knight;
+    this.rows = 8;
+    this.columns = 8;
+    this.visitedSpaces = [];
+    this.turns = 0;
+
+    this.isLegalMove = function isLegalMove(space) {
+        return this.hasSpaceBeenVisited(space) == false && Math.abs(space.x - knight.currentPosition.x) == 2 && Math.abs(space.y - knight.currentPosition.y) == 1
+            || this.hasSpaceBeenVisited(space) == false && Math.abs(space.x - knight.currentPosition.x) == 1 && Math.abs(space.y - knight.currentPosition.y) == 2
+            || this.turns == 0
+    }
+
+    this.addToVisitedSpaces = function addToVisitedSpaces(space) {
+        this.visitedSpaces.push(space);
+        this.turns++;
+    }
+
+    this.hasSpaceBeenVisited = function hasSpaceBeenVisited(space) {
+        for (var i = 0; i < this.visitedSpaces.length; i++)
+            if (space.x === this.visitedSpaces[i].x && space.y == this.visitedSpaces[i].y)
+                return true;
+
+        return false;
+    }
+
+    this.undoMove = function undoMoce() {
+        this.visitedSpaces.pop();
+        this.turns--;
+
+        return this.visitedSpaces[this.visitedSpaces.length - 1];
+    }
+}
---
+++ knight.js
@@ -0,0 +1,8 @@
+﻿function Knight() {
+    this.currentPosition = new Coordinate();
+
+    this.visitSpace = function visitSpace(space) { 
+        this.currentPosition.x = space.x;
+        this.currentPosition.y = space.y;
+    }
+}
